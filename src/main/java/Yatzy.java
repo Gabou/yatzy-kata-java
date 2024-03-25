258ed4a64844e84edbb3cd58e04e7bdf4bb2c1ae
Gabou <supergabou+git@outlook.fr>
2024-03-25 15:18:54 +0100

    refactoring small straight and large straight

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -102,38 +102,20 @@ public class Yatzy {
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
-
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
-        return 0;
+        List<Integer> dice = Arrays.asList(d1, d2, d3, d4, d5);
+        return computeStraight(dice);
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
+        List<Integer> dice = Arrays.asList(d1, d2, d3, d4, d5);
+        return computeStraight(dice);
+    }
+
+    private static int computeStraight(List<Integer> dice) {
+        if (dice.stream().distinct().count() == 5) {
+            return dice.stream().mapToInt(Integer::intValue).sum();
+        }
         return 0;
     }
 
